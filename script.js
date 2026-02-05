55901d3ca39d206348f11f53e7bb3e6007a8e28e
sara19git <sarabenlalla9@gmail.com>
2026-02-05 20:03:26 +0100

    added updates on js functionality

--- script.js
+++ script.js
@@ -6,9 +6,12 @@ let addition = document.getElementById('addition');
 let substraction =  document.getElementById('substraction');
 let multiplication = document.getElementById('multiplication');
 let division = document.getElementById('division');
-let delelte = document.getElementById('')
+let delelte = document.getElementById('delelte');
 let currentValue = '';
 let operation;
+
+let isResultDisplay = false;
+let displayValue = '';
 
 let firstNum;
 let secondNum;
@@ -30,6 +33,7 @@ function div(num1, num2){
 equal.addEventListener('click', function(){
     if (!operation || currentValue === '') return;
     secondNum = Number(currentValue);
+    console.log("secondNum: " + secondNum);
     if(operation === '+'){
         result.textContent = sum(firstNum, secondNum);
     }else if(operation === '-'){
@@ -44,18 +48,29 @@ equal.addEventListener('click', function(){
         }
         result.textContent = div(firstNum, secondNum);
     }
-
     currentValue = result.textContent;
+    displayValue = result.textContent;
+    isResultDisplay = true;
     operation = null;
+    console.log('displayValue : ' + displayValue );
 })
 
 
 currentNum.forEach(btn => {
     btn.addEventListener('click', function(event){
-        if (event.target.textContent === '.' && currentValue.includes('.')) return;
-        currentValue = currentValue + event.target.textContent ;
-        result.textContent = currentValue;
-    })
+       if ( event.target.textContent === '.' && currentValue.includes('.')) return;
+       if (isResultDisplay) {
+            currentValue = event.target.textContent;
+            displayValue = currentValue;   
+            isResultDisplay = false;
+        } else {
+            currentValue = currentValue + event.target.textContent;
+            displayValue = displayValue + event.target.textContent;
+        }
+        result.textContent = displayValue;
+        console.log('displayValue : ' + displayValue );
+
+    });
 })
 
 
@@ -63,36 +78,45 @@ addition.addEventListener('click', function(){
     if (currentValue === '') return;
     firstNum = Number(currentValue);
     operation = "+";
+    displayValue = displayValue + ' + ';
+    result.textContent = displayValue;
     currentValue = '';
-    result.textContent = '';
+    console.log("displayValue: " + displayValue);
 })
 
 substraction.addEventListener('click', function(){
     if (currentValue === '') return;
     firstNum = Number(currentValue);
     operation = "-";
+    displayValue = displayValue + ' - ';
+    result.textContent = displayValue;
     currentValue = '';
-    result.textContent = '';
+    console.log("displayValue: " + displayValue);
 })
 
 multiplication.addEventListener('click', function(){
     if (currentValue === '') return;
     firstNum = Number(currentValue);
     operation = "*";
+    displayValue = displayValue + ' * ';
+    result.textContent = displayValue;
     currentValue = '';
-    result.textContent = '';
+    console.log("displayValue: " + displayValue);
 })
 
 division.addEventListener('click', function(){
     if (currentValue === '') return;
     firstNum = Number(currentValue);
     operation = "/";
+    displayValue = displayValue + ' / ';
+    result.textContent = displayValue;
     currentValue = '';
-    result.textContent = '';
+    console.log("displayValue: " + displayValue);
 })
 
 clear.addEventListener('click', function () {
     currentValue = '';
+    displayValue = '';
     firstNum = null;
     secondNum = null;
     operation = null;
