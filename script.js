12b50153a32c1cf3a253a14cff04a9623c1bdc37
sara19git <sarabenlalla9@gmail.com>
2026-03-01 22:22:06 +0100

    priority operators functionality added

--- script.js
+++ script.js
@@ -11,67 +11,87 @@ let currentValue = '';
 let isResultDisplay = false;
 let displayValue = '';
 
-let inputs = [];
-let operationsArray = [];
+let mathExpression = [];
 
-function sum(num1, num2) {
-    return num1 + num2;
-}
-function sub(num1, num2) {
-    return num1 - num2;
-}
-function mult(num1, num2){
-    return num1 * num2;
-}
+function evaluate(arr){
 
-function div(num1, num2){
-    return num1 / num2;
+    let temp = [...arr];
+
+    /** high priority */
+    for(let i = 0 ; i<temp.length ; i++){
+        if(temp[i] === '*' || temp[i] === '/'){
+            let result;
+
+            if(temp[i] === '*'){
+                result = temp[i-1] * temp[i+1];
+            }else {
+                if(temp[i+1] === 0) {
+                    return 'Error';
+                }
+                result = temp[i-1] / temp[i+1];
+            }
+
+            temp.splice(i-1 , 3 , result);
+            i--;
+        }
+
+    }
+
+    /** low priority */
+
+    for(let i = 0; i < temp.length; i++){
+        if(temp[i] === '+' || temp[i] === '-'){
+            let result;
+
+            if(temp[i] === '+'){
+                result = temp[i-1] + temp[i+1];
+            }else {
+                result = temp[i-1] - temp[i+1];
+            }
+
+            temp.splice(i-1 , 3 , result);
+            i--;
+        }
+    }
+
+    return temp[0];
 }
 
 equal.addEventListener('click', function(){
-    if (currentValue === '') return;
-    inputs.push(Number(currentValue));
-    console.log("second number: ", inputs);
-    let firstNum = inputs[0];
-    for(let i = 0 ; i < operationsArray.length; i++){
-        let secondNum = inputs[i + 1];
-        if(operationsArray[i] === '+'){
-        firstNum = sum(firstNum, secondNum);
-        }else if(operationsArray[i] === '-'){
-            firstNum = sub(firstNum, secondNum);
-        }else if(operationsArray[i] === '*'){
-            firstNum = mult(firstNum, secondNum);
-        }else if (operationsArray[i] === '/') {
-            if (inputs[i+1] === 0) {
-                result.textContent = 'Error';
-                currentValue = '';
-                return;
-            }
-            firstNum = div(firstNum, secondNum);
-        }
-    }
-    console.log(firstNum);
-    result.textContent = firstNum;
-    
-    currentValue = result.textContent.toString();
-    displayValue = result.textContent.toString();
-    inputs = [];
-    operationsArray = [];
+
+    if(currentValue === '') return;
+
+    mathExpression.push(Number(currentValue));
+
+    let finalResult = evaluate(mathExpression);
+
+    result.textContent = finalResult;
+    console.log('result: ' +finalResult);
+    currentValue = finalResult.toString();
+    displayValue = finalResult.toString();
+    mathExpression = [];
     isResultDisplay = true;
+
+   
 })
 
-
+// loop over each button number
 currentNum.forEach(btn => {
     btn.addEventListener('click', function(event){
+        // prevent double "." click 
        if ( event.target.textContent === '.' && currentValue.includes('.')) return;
+       //check if the result is diplayed on the screen 
+       // if isResultDisplay = true enter a new number then turn isResultDisplay = false
        if (isResultDisplay) {
             currentValue = event.target.textContent;
             displayValue = currentValue;   
             isResultDisplay = false;
+            // else is the ordinary state means enter numbers normally
         } else {
             currentValue = currentValue + event.target.textContent;
             displayValue = displayValue + event.target.textContent;
         }
+        // show the result on the screen
         result.textContent = displayValue;
 
     });
@@ -80,10 +100,8 @@ currentNum.forEach(btn => {
 
 function operations(op){
     if (currentValue === '') return;
-    inputs.push(Number(currentValue));
-    console.log("first number: ", inputs);
-    operationsArray.push(op);
-    console.log(operationsArray);
+    mathExpression.push(Number(currentValue));
+    mathExpression.push(op);
     displayValue = displayValue + ' ' + op + ' ';
     result.textContent = displayValue;
     currentValue = '';
@@ -99,9 +117,7 @@ division.addEventListener('click', ()=> operations("/"));
 clear.addEventListener('click', function () {
     currentValue = '';
     displayValue = '';
-    firstNum = null;
-    secondNum = null;
-    operation = null;
+    mathExpression = [];
     result.textContent = '';
 });
 
